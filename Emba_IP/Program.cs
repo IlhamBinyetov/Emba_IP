795845b7a175ccdf2ebcd22710f3bc3ff37cbf5a
ilhambinyatov <ilhamgb@code.edu.az>
2025-07-26 22:00:42 +0400

    Adding new packages

--- Emba_IP/Program.cs
+++ Emba_IP/Program.cs
@@ -1,10 +1,22 @@
+using Emba_IP.DB;
 using Emba_IP.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IpFileService>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
 
 var app = builder.Build();
 
